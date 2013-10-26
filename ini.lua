0d422f06fea76cb010d2173360951899424770d2
Jens John <jens.o.john@gmail.com>
2013-10-26 16:21:14 +0900

    Updated copyright notice

--- ini.lua
+++ ini.lua
@@ -1,7 +1,7 @@
 #!/usr/bin/luajit
 --[[
     ini.lua - read/write access to INI files in Lua
-    Copyright (C) 2013 Jens Oliver John <asterisk@2ion.de>
+    Copyright (C) 2012-2013 Jens Oliver John <asterisk@2ion.de>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
